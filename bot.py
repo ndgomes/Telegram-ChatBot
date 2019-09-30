7c2150ddaef12225660b8e7c74f8925120aecacb
frznn <fryzenn@gmail.com>
2019-10-01 00:44:12 +0100

    add Lib Telebot

--- bot.py
+++ bot.py
@@ -31,15 +31,15 @@ def send_help(message):
 def show_menu(message):
     chatid = message.chat.id
     markup_menu = types.ReplyKeyboardMarkup(row_width=4)
-    itembtn1 = types.KeyboardButton('Button 1',)
-    itembtn2 = types.KeyboardButton('Button 2')
-    itembtn3 = types.KeyboardButton('Button 3')
-    itembtn4 = types.KeyboardButton('Button 4')
-    markup_menu.add(itembtn1, itembtn2, itembtn3,itembtn4)
+    button1 = types.KeyboardButton('Button 1',)
+    button2 = types.KeyboardButton('Button 2')
+    button3 = types.KeyboardButton('Button 3')
+    button4 = types.KeyboardButton('Button 4')
+    markup_menu.add(button1, button2, button3, button4)
     bot.send_message(chatid, "Choose one option from the menu", reply_markup=markup_menu)
  
 @bot.message_handler(func=lambda msg: msg.text is not None and 'Button 1' in msg.text)
-def echo1mes(message):
+def echo(message):
     chatid = message.chat.id
     bot.send_message(chatid,"You just clicked on mes 1", reply_markup=markup)
     nome = bot.send_message(chatid,"Escolha 1/2/3 ")
